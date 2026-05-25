fceadb0fcf394294728e8a3698660adc994983e6
junior700.eletro-png <junior700.eletro@gmail.com>
2026-05-25 01:04:29 -0300

    Atualizar repositorio

--- Agente_Independente/1/src/fixer/__init__.py
+++ Agente_Independente/1/src/fixer/__init__.py
@@ -1,22 +1,4 @@
-﻿"""Módulo fixer: lógica de correção de código Python."""
+﻿# __init__.py
+from .core_v2 import FixerCoreV2, FixResult
 
-from .core import PythonFixer
-from .agent import FixerAgent
-
-# Path: C:\Users\user\Desktop\Tools\Agente_Independente\0\src\fixer\__init__.py
-# Name: __init__.py - Centralizador de exports
-
-from core_v3_final import (
-    CacheManager,
-    MetricsLogger,
-    fallback_correct,
-    FixerCoreV3Final,
-    # ... adicione todas as classes/funções que core_v3_final exporta
-)
-
-__all__ = [
-    'CacheManager',
-    'MetricsLogger',
-    'fallback_correct',
-    'FixerCoreV3Final',
-]
+__all__ = ["FixerCoreV2", "FixResult"]
