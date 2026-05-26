6e8ba128442ebc33316fb71cbf1a76e9da2cc55b
junior700.eletro-png <junior700.eletro@gmail.com>
2026-05-26 14:49:35 -0300

    Atualizar repositorio

--- Agente_Independente/1/src/backend.bat
+++ Agente_Independente/1/src/backend.bat
@@ -1,6 +1,6 @@
 @echo off
 REM powershell -ExecutionPolicy Bypass -File "C:\Users\user\Desktop\Tools\Copiar_recriar_projetos\fixo_atua_remoto\bootstrap_xmlv1.ps1"
-powershell -STA -File .\bootstrap_xmlv2.ps1
-REM cd C:\Users\user\Desktop\Tools\Agente_Independente\1\src
-REM python backend.py
+REM powershell -STA -File .\bootstrap_xmlv2.ps1
+cd C:\Users\user\Desktop\Tools\Agente_Independente\1\src
+python backend.py
 pause
